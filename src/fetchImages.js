5b2a23e89182776ae757dc450d1b4f3b13fc7e90
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-05-14 22:36:14 +0200

    working

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -2,34 +2,22 @@ export { fetchImages };
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const search_window = document.querySelector('input[name="searchQuery"]');
-var API_KEY = '36382729-c052200aae6cb40748bb0f135';
-var URL =
-  'https://pixabay.com/api/?key=' +
-  API_KEY +
-  '&q=' +
-  encodeURIComponent(search_window.value);
+const API_KEY = '36382729-c052200aae6cb40748bb0f135';
+const API_URL = 'https://pixabay.com/api/';
 
-const searchTerm = search_window.value.trim();
+async function fetchImages(searchTerm, page) {
+  const config = {
+    params: {
+      key: API_KEY,
+      q: searchTerm,
+      image_type: 'photo', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
+      orientation: 'horizontal', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
+      safesearch: true,
+      per_page: 40,
+      page: page,
+    },
+  };
 
-const config = {
-  params: {
-    key: '36382729-c052200aae6cb40748bb0f135',
-    q: searchTerm,
-    image_type: 'photo', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
-    orientation: 'horizontal', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
-    safesearch: true,
-    per_page: 40,
-    page: 1,
-  },
-};
-
-const fetchImages = async () => {
-  try {
-    const response = await axios.get(URL, config);
-
-    Notiflix.Notify.info(`${response.data}`);
-  } catch (error) {
-    Notiflix.Notify.warning('Sorry, something went wrong');
-  }
-};
+  //pobieramy zdjecia
+  return await axios.get(API_URL, config);
+}
--- src/index.js
+++ src/index.js
@@ -6,32 +6,70 @@ import { fetchImages } from './fetchImages';
 
 const search_window = document.querySelector('input[name="searchQuery"]');
 const search_btn = document.querySelector('button[type="submit"]');
-const imagesForm = document.getElementById('#search-form');
+const imagesForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-const searchTerm = search_window.value.trim();
+const loadMore = document.getElementById('load-more');
 
-imagesForm.addEventListener('submit', handleInput(searchTerm));
-console.log(imagesForm);
+let currentPage = 1;
+let searchQuery = '';
+let images = [];
 
-function handleInput(searchTerm) {
-  if (searchTerm) {
-    gallery.innerHTML = '';
-    $.getJSON(URL, function (data) {
-      if (parseInt(data.totalHits) > 0)
-        Notiflix.Notify.info(`Znaleźliśmy ${data.totalHits} wyszukań`);
-      //     $.each(data.hits, function (i, hit) {
-      //       console.log(hit.pageURL);
-      //     });
-      //   } else Notiflix.Notify.alert('No hits');
-      // });
-      else Notiflix.Notify.alert('No hits');
+// dodajemy obsluge do przycisku pobrania zdjec w formularzu
+imagesForm.addEventListener('submit', e => {
+  e.preventDefault();
+  searchQuery = search_window.value;
+
+  fetchImages(searchQuery, currentPage)
+    .then(function (response) {
+      images = response.data.hits;
+      currentPage = 1;
+
+      if (images.length == 0) {
+        Notiflix.Notify.failure("There wasn't any images matching search.");
+        return;
+      }
+
+      renderImages(images);
+
+      Notiflix.Notify.info(
+        `Udało się wyszukać : ${response.data.total} wyszukań`
+      );
+    })
+    .catch(function (error) {
+      Notiflix.Notify.failure('There was any error in request to api.');
     });
-    fetchImages(searchTerm);
-    renderImages(data);
-  } else gallery.innerHTML = '';
-}
+});
+
+// dodajemy obsluge do przycisku ladowania kolejnych zdjec
+loadMore.addEventListener('click', e => {
+  // zwiekszamy numer strony
+  currentPage += 1;
+  //   ukrywamy przycisk ładowania kolejnych zdjec
+  loadMore.classList.add('hidden');
+
+  //   pobieramy zdjecia
+  fetchImages(searchQuery, currentPage)
+    .then(function (response) {
+      let newImages = response.data.hits;
+
+      //   jesli nie mamy zadnych zdjec wiecej
+      if (newImages.length == 0) {
+        Notiflix.Notify.failure('There was any error in request to api.');
+        return;
+      }
+
+      images = images.concat(newImages);
+
+      renderImages(images);
+    })
+    .catch(function (error) {
+      Notiflix.Notify.failure("There wasn't any images matching search.");
+    });
+});
 
 function renderImages(data) {
+  gallery.classList.remove('hidden');
+  loadMore.classList.remove('hidden');
   const galleryImages = data
     .map(
       ({
@@ -43,25 +81,39 @@ function renderImages(data) {
         comments,
         downloads,
       }) => {
-        return `<div class="photo-card">
-       <a class="image-link href="${largeImageURL}> <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads ${downloads}</b>
-          </p>
-        </div>
-      </div>`;
+        return `
+        <div class="photo-card">
+            <a class="image-link" href="${largeImageURL}"> 
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes ${likes}</b>
+                </p>
+                <p class="info-item">
+                    <b>Views ${views}</b>
+                </p>
+                <p class="info-item">
+                    <b>Comments ${comments}</b>
+                </p>
+                <p class="info-item">
+                    <b>Downloads ${downloads}</b>
+                </p>
+            </div>
+        </div>`;
       }
     )
     .join('');
   gallery.innerHTML = galleryImages;
+
+  createPreviewOfImages();
+}
+
+function createPreviewOfImages() {
+  const lightbox = new SimpleLightbox('.gallery .photo-card a  ', {
+    captions: true,
+    captionsData: 'alt',
+    captionsPosition: 'bottom',
+    captionDeley: 250,
+  });
 }
