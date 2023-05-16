1a54a0e8b4a1c51d7155d0e234b6b56f61d0947e
KamilD90 <117952413+KamilD90@users.noreply.github.com>
2023-05-16 20:26:21 +0200

    cleaning

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,27 +1,5 @@
 export { fetchImages };
 import axios from 'axios';
-
-// const API_KEY = '36382729-c052200aae6cb40748bb0f135';
-// const API_URL = 'https://pixabay.com/api/';
-
-// async function fetchImages(searchTerm, page) {
-//   const config = {
-//     params: {
-//       key: API_KEY,
-//       q: searchTerm,
-//       image_type: 'photo', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
-//       orientation: 'horizontal', // wartość musi być przekazana jako ciąg znaków w cudzysłowie
-//       safesearch: true,
-//       per_page: 40,
-//       page: page,
-//     },
-//   };
-
-//   //pobieramy zdjecia
-//   return await axios.get(API_URL, config);
-// }
-
-import axios from 'axios';
 import Notiflix from 'notiflix';
 
 const API_KEY = '36382729-c052200aae6cb40748bb0f135';
